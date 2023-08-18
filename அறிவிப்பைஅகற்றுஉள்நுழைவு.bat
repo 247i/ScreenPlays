3aa59efb5aa81e091358d167d2bb097967642fd1
தமிழ்நேரம் <anishprabu.t@gmail.com>
2023-08-18 09:39:24 +0530

    Create அறிவிப்பைஅகற்றுஉள்நுழைவு.bat

---
+++ அறிவிப்பைஅகற்றுஉள்நுழைவு.bat
@@ -0,0 +1,2 @@
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v legalnoticecaption /d "" /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v legalnoticetext /d "" /f
